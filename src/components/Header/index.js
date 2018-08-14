a044e26b9ac07af355cd3655ee8fb0f413eb5fac
Naveed-rana <rana.naveed812@gmail.com>
2018-08-14 11:46:52 +0500

    complete the header components style and links

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,28 +3,33 @@ import Paper from '@material-ui/core/Paper';
 import Hidden from '@material-ui/core/Hidden';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import {BrowserRouter as Router, Link} from 'react-router-dom';
+import Icon from '@material-ui/core/Icon';
 
 class HeaderBar extends Component {
     render() {
         return (
             <div>
-                <Paper className="" elevation={5}>
+                <Router>
+                <Paper className="headerFix" elevation={1}>
                  <Grid container spacing={8}>
                    <Hidden smDown>
-                   <Grid item xs={12} md={9}>
-                     
+                   <Grid item xs={12} md={9} >
+                     <Typography variant="caption" className="paddingLeft" > 
+                     <Link to="">Daily Ads</Link> | <Link to="">Posting</Link> |  <Link to="">Help</Link> | <Link to="">Contact</Link>
+                     </Typography>
                    </Grid>
                    </Hidden> 
-                   <Grid item xs={12} md={3}>
+                   <Grid item xs={12} md={3} align="center">
                      <Typography variant="caption" > 
-                     Hi! <a href="">Sign in</a> or <a href="">register</a>
+                     Hi! <Link to=""> Sign in </Link> or <Link to=""> Register </Link>
+                     
                      </Typography>
-                     <Typography variant="caption" > 
-                       
-                     </Typography>
+            
                    </Grid>
                  </Grid>
                 </Paper>
+                </Router>
             </div>
         )
     }
