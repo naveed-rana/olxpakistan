61c7653f932165583167964a3d40b3fe8fed2273
Naveed-rana <rana.naveed812@gmail.com>
2018-08-15 12:51:07 +0500

    add paper into positng component

--- src/components/Posting/index.js
+++ src/components/Posting/index.js
@@ -1,10 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import Hidden from '@material-ui/core/Hidden';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
 
 class Posting extends Component {
     render() {
         return (
             <div>
-               posting 
+            <Grid container spacing={8}> 
+              <Hidden smDown>
+              <Grid item xs={1} md={2}>
+              </Grid>
+              </Hidden>
+              <Grid item xs={12} md={8}>
+                <Paper className="postingPaper" elevation={5}>
+                 <Typography variant="body2" align="center"> 
+                   POST AN ADD
+                 </Typography>
+                   <Divider />
+                </Paper>
+              </Grid>
+              <Hidden smDown>
+              <Grid item xs={1} md={2}>
+              </Grid>
+              </Hidden>
+            </Grid>
             </div>
         )
     }
