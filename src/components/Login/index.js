dadedfec419c267ffe48b072189f2669bd2b6c44
Naveed-rana <rana.naveed812@gmail.com>
2018-08-17 12:26:50 +0500

    working signup form

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
@@ -11,107 +11,127 @@ import Checkbox from '@material-ui/core/Checkbox';
 import {Link} from 'react-router-dom';
 
 class Login extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    onChangeHandler = (e) => {
+        let name = e.target.name;
+        let value = e.target.value;
+        this.setState({[name]: value});
+    }
+
     render() {
+        const {email,password} = this.state;
+        const isnotValid = email ==='' || password ==='';  
+        console.log(this.state);
         return (
             <div>
-                <Grid container spacing={8}> 
-                <Hidden smDown>
-                <Grid item xs={4} md={4}> 
-                </Grid>
-                </Hidden>
-                
-                 <Grid item xs={12} md={4}>
-                   <Paper className="loginPaper" elevation={10}>
-                     
-                     <Typography variant="body2" align="center">
-                      Login<i className="material-icons iconSize">search
-                </i>
-                     </Typography>  
-                     <Grid container spacing={8} className="LoginContainer"> 
-                       <Grid item xs={12} md={12} className="paddingTop">
-                       <TextField
-                       type="email"
-                    InputProps={{
-                      startAdornment: (
-                        <InputAdornment position="start">
-                          <i className="material-icons iconFixfield">
-                              account_box
-                           </i>
-                        </InputAdornment>
-                      ),
-                    }}
-                  fullWidth={true}
-                  required={true}
-                  helperText=""
-                  placeholder="Email"
-                  />
-                       </Grid>
-                       <Grid item xs={12} md={12} className="paddingTop">
-                       <TextField
-                       type="password"
-                    InputProps={{
-                      startAdornment: (
-                        <InputAdornment position="start">
-                          <i className="material-icons iconFixfield">
-                              account_box
-                           </i>
-                        </InputAdornment>
-                      ),
-                    }}
-                  fullWidth={true}
-                  required={true}
-                  placeholder="Password"
-                  />
-                       </Grid>
-                        
-            <Grid item xs={12} md={12} align="right" className="paddingTop">
-            <FormControlLabel
-            control={
-            <Checkbox
-              value="checkedB"
-              color="primary"
-            />
-          }
-          label="Remember Me"
-            />
-            </Grid>
+                <Grid container spacing={8}>
+                    <Hidden smDown>
+                        <Grid item xs={4} md={4}></Grid>
+                    </Hidden>
 
-                <Grid item xs={12} md={12} align="center" >
-                  <Button variant="contained" size="small" color="primary" className="loginbtn">
-                  Submit
-                  <i className="material-icons iconSize">
-                send
-                </i>
-                </Button>
-                  </Grid>
-                  <Grid item xs={12} md={6} className="paddingTop">
-                    <Typography variant="body2" > 
-                      <Link to="">Register</Link>
-                    </Typography>
-                  </Grid>
-                   <Hidden smDown>
-                   <Grid item xs={12} md={6} align="right" className="paddingTop">
-                  <Typography variant="body2" > 
-                      <Link to="/">Forget Password</Link>
-                    </Typography>
-                  </Grid>
-                   </Hidden>
-                   <Hidden mdUp>
-                   <Grid item xs={12} md={6}>
-                  <Typography variant="body2" className="paddingTop" > 
-                      <Link to="">Forget Password</Link>
-                    </Typography>
-                  </Grid>
-                   </Hidden>
-                     </Grid>
-                 
-                   </Paper> 
-                 </Grid>
+                    <Grid item xs={12} md={4}>
+                        <Paper className="loginPaper" elevation={10}>
 
-                <Hidden smDown>
-                <Grid item xs={4} md={4}> 
-                </Grid>
-                </Hidden>
+                            <Typography variant="display1" align="center">
+                               <i className="material-icons largeIcon">account_box
+                                </i>LOGIN
+                            </Typography>
+                            <Grid container spacing={8} className="LoginContainer">
+                                <form>
+                                    <Grid item xs={12} md={12} className="paddingTop">
+                                        <TextField
+                                            type="email"
+                                            name="email"
+                                            InputProps={{
+                                            startAdornment: (
+                                                <InputAdornment position="start">
+                                                    <i className="material-icons iconFixfield">
+                                                        email
+                                                    </i>
+                                                </InputAdornment>
+                                            )
+                                        }}
+                                            fullWidth={true}
+                                            required={true}
+                                            helperText=""
+                                            placeholder="Email"
+                                            onChange={this.onChangeHandler}
+                                            />
+                                    </Grid>
+                                    <Grid item xs={12} md={12} className="paddingTop">
+                                        <TextField
+                                            type="password"
+                                            name="password"
+                                            InputProps={{
+                                            startAdornment: (
+                                                <InputAdornment position="start">
+                                                    <i className="material-icons iconFixfield">
+                                                        lock
+                                                    </i>
+                                                </InputAdornment>
+                                            )
+                                        }}
+                                            fullWidth={true}
+                                            required={true}
+                                            placeholder="Password"
+                                            onChange={this.onChangeHandler}
+                                            />
+                                    </Grid>
+
+                                    <Grid item xs={12} md={12} align="right" className="paddingTop">
+                                        <FormControlLabel
+                                            control={< Checkbox value = "checkedB" color = "primary" />}
+                                            label="Remember Me"/>
+                                    </Grid>
+
+                                    <Grid item xs={12} md={12} align="center">
+                                        <Button
+                                        type="submit"
+                                        disabled={isnotValid}
+                                        variant="contained" size="small" color="primary" className="loginbtn">
+                                            Submit
+                                            <i className="material-icons iconSize">
+                                                send
+                                            </i>
+                                        </Button>
+                                    </Grid>
+                                    <Grid container spacing={8} className="paddingTop"> 
+                                    <Grid item xs={12} md={6} className="paddingTop">
+                                        <Typography variant="body2">
+                                            <Link to="">Register</Link>
+                                        </Typography>
+                                    </Grid>
+                                    <Hidden smDown>
+                                        <Grid item xs={12} md={6} align="right" className="paddingTop">
+                                            <Typography variant="body2">
+                                                <Link to="/">Forget Password</Link>
+                                            </Typography>
+                                        </Grid>
+                                    </Hidden>
+                                    <Hidden mdUp>
+                                        <Grid item xs={12} md={6}>
+                                            <Typography variant="body2" className="paddingTop">
+                                                <Link to="">Forget Password</Link>
+                                            </Typography>
+                                        </Grid>
+                                    </Hidden>
+                                    </Grid>
+                                </form>
+                            </Grid>
+
+                        </Paper>
+                    </Grid>
+
+                    <Hidden smDown>
+                        <Grid item xs={4} md={4}></Grid>
+                    </Hidden>
                 </Grid>
             </div>
         )
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -6,36 +6,57 @@ import Hidden from '@material-ui/core/Hidden';
 import TextField from '@material-ui/core/TextField';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import Button from '@material-ui/core/Button';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import {Link} from 'react-router-dom';
+import Map from ''
 
 class SignUp extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: '',
+            email: '',
+            password: '',
+            confirmpassword: '',
+            cellNo:'',
+            address: '',
+            city:'',
+            province:'',
+            country:''
+        }
+    }
+
+    onChangeHandler = (e) => {
+        let name = e.target.name;
+        let value = e.target.value;
+        this.setState({[name]: value});
+    }
+
     render() {
         return (
             <div>
                 <Grid container spacing={8}> 
                 <Hidden smDown>
-                <Grid item xs={4} md={4}> 
+                <Grid item xs={4} md={3}> 
                 </Grid>
                 </Hidden>
                 
-                 <Grid item xs={12} md={4}>
+                 <Grid item xs={12} md={6}>
                    <Paper className="loginPaper" elevation={10}>
                      
-                     <Typography variant="body2" align="center">
-                      SignUp<i className="material-icons iconSize">search
-                </i>
-                     </Typography>  
+                     <Typography variant="display1" align="center">
+                     <i className="material-icons largeIcon">account_box
+                    </i>
+                    SIGNUP
+                    </Typography>  
                      <Grid container spacing={8} className="LoginContainer"> 
-                       <Grid item xs={12} md={12} className="paddingTop">
+                       <Grid item xs={12} md={6} className="paddingTop">
                        <TextField
-                       type="email"
+                
                     InputProps={{
                       startAdornment: (
                         <InputAdornment position="start">
                           <i className="material-icons iconFixfield">
-                              account_box
+                          account_circle
                            </i>
                         </InputAdornment>
                       ),
@@ -43,73 +64,83 @@ class SignUp extends Component {
                   fullWidth={true}
                   required={true}
                   helperText=""
-                  placeholder="Email"
+                  placeholder="Name"
+                  name="name"
+                  onChange={this.onChangeHandler}
                   />
                        </Grid>
-                       <Grid item xs={12} md={12} className="paddingTop">
+                       <Grid item xs={12} md={6} className="paddingTop">
                        <TextField
-                       type="password"
+                       type="email"
                     InputProps={{
                       startAdornment: (
                         <InputAdornment position="start">
                           <i className="material-icons iconFixfield">
-                              account_box
+                              email
                            </i>
                         </InputAdornment>
                       ),
                     }}
                   fullWidth={true}
                   required={true}
-                  placeholder="Password"
+                  placeholder="email"
+                  name="email"
+                  onChange={this.onChangeHandler}
                   />
                        </Grid>
-                        
-            <Grid item xs={12} md={12} align="right" className="paddingTop">
-            <FormControlLabel
-            control={
-            <Checkbox
-              value="checkedB"
-              color="primary"
-            />
-          }
-          label="Remember Me"
-            />
-            </Grid>
+                    
+                    <Grid item xs={12} md={6} className="paddingTop">
+                    <TextField
+                    
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <i className="material-icons iconFixfield">
+                              lock
+                           </i>
+                        </InputAdornment>
+                      ),
+                    }}
+                  type="password"
+                  fullWidth={true}
+                  required={true}
+                  placeholder="Password"
+                  name="password"
+                  onChange={this.onChangeHandler}
+                  />
+                    </Grid>
 
-                <Grid item xs={12} md={12} align="center" >
-                  <Button variant="contained" size="small" color="primary" className="loginbtn">
-                  Submit
-                  <i className="material-icons iconSize">
-                send
-                </i>
-                </Button>
-                  </Grid>
-                  <Grid item xs={12} md={6} className="paddingTop">
-                    <Typography variant="caption" > 
-                      <Link to="">Register</Link>
-                    </Typography>
-                  </Grid>
-                   <Hidden smDown>
-                   <Grid item xs={12} md={6} align="right" className="paddingTop">
-                  <Typography variant="caption" > 
-                      <Link to="/">Forget Password</Link>
-                    </Typography>
-                  </Grid>
-                   </Hidden>
-                   <Hidden mdUp>
-                   <Grid item xs={12} md={6}>
-                  <Typography variant="caption" className="paddingTop" > 
-                      <Link to="">Forget Password</Link>
-                    </Typography>
-                  </Grid>
-                   </Hidden>
+                     <Grid item xs={12} md={6} className="paddingTop">
+                    <TextField
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <i className="material-icons iconFixfield">
+                              lock
+                           </i>
+                        </InputAdornment>
+                      ),
+                    }}
+                  type="password"
+                  fullWidth={true}
+                  required={true}
+                  placeholder="Confirm Password"
+                  name="confirmpassword"
+                  onChange={this.onChangeHandler}
+                  />
+                    </Grid>
+
+                    <Grid item xs={12} md={12} className="paddingTop">
+                      <Map underline={true} />
+                    </Grid>
+
                      </Grid>
                  
                    </Paper> 
                  </Grid>
 
                 <Hidden smDown>
-                <Grid item xs={4} md={4}> 
+                <Grid item xs={4} md={3}> 
                 </Grid>
                 </Hidden>
                 </Grid>
